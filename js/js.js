601e463c0b4bd9abe1950b012c46d110e05c655d
keven-shtk <keven.elpidio@etec.sp.gov.br>
2020-11-06 11:45:21 -0300

    Commit 5

--- js/js.js
+++ js/js.js
@@ -1,24 +1,83 @@
 $(document).ready(function(){
+    $('.musica').fadeToggle('show');
 
-    $('.btn-find').click(function(e){
-        e.preventDefault()
+    $('#btn-1-5em').click(function(){
+        $('p').removeClass('font-2-0');
+        $('p').removeClass('font-2-5');
+        $('p').addClass('font-1-5');
+    });
+    $('#btn-2-0em').click(function(){
+        $('p').removeClass('font-1-5');
+        $('p').removeClass('font-2-5');
+        $('p').addClass('font-2-0');
+    });
+    $('#btn-2-5em').click(function(){
+        $('p').removeClass('font-2-5');
+        $('p').removeClass('font-2-0');
+        $('p').addClass('font-2-5');
+    });
 
-        var cep = $('#CEP').val()
+    $('#btn-e').click(function(){
+        $('.imagem').fadeOut(2500);
+        $('.menu').fadeToggle('slow');
+    });
+    $('#btn-m').click(function(){
+        $('.imagem').fadeIn(2500);
+        $('.menu').fadeToggle('slow');
+    });
 
-        var find = `https://viacep.com.br/ws/${cep}/json`
- 
-        console.log(find)
-        $.ajax({
-            dataType:'JSON',
-            type: 'GET',
-            assync: true,
-            url: find,
-            success: function(dados){
-                $('#longradouro').val(dados.longradouro)
-                $('#bairro').val(dados.bairro)
-                $('#localidade').val(dados.localidade)
-                $('#uf').val(dados.uf)
-            }
-        })
-    })
-})
+    $('.menu').hide();
+
+    $('.nav-bar').click(function(){
+        $('.menu').fadeToggle('slow');
+    });
+
+    $('#btn-s').click(function() {
+        $('body').addClass('img2'); 
+        $('p').removeClass('font-1-5');
+        $('p').removeClass('font-2-0');
+        $('p').removeClass('font-2-5');
+        $('.nav-bar').fadeOut(100);
+        $('.extra').fadeOut(100);
+        $('.imagem').fadeOut(100);
+        $('p').fadeOut(100);
+        $('.imagem').fadeOut(2500);
+        $('.musica').fadeIn(100);
+  
+     
+        $('.imagem2').fadeIn(10000);
+        $('.imagem2').fadeOut(1000);
+        $('.imagem2').fadeIn(10000);
+        $('.imagem2').fadeOut(1000);
+        $('.imagem2').fadeIn(10000);
+        $('.imagem2').fadeOut(1000);
+        $('.imagem2').fadeIn(10000);
+        $('.imagem2').fadeOut(1000);
+        $('.imagem2').fadeIn(10000);
+        $('.imagem2').fadeOut(1000);
+        $('.imagem2').fadeIn(10000);
+        $('.imagem2').fadeIn(10000);
+        $('.imagem2').fadeOut(1000);
+        $('.imagem2').fadeIn(10000);
+        $('.imagem2').fadeOut(1000);
+        $('.imagem2').fadeIn(10000);
+
+        $('.imagem3').fadeIn(1000);
+        $('.imagem3').fadeOut(10000);
+        $('.imagem3').fadeIn(1000);
+        $('.imagem3').fadeOut(10000);
+        $('.imagem3').fadeIn(10000);
+        $('.imagem3').fadeOut(1000);
+        $('.imagem3').fadeIn(1000);
+        $('.imagem3').fadeOut(10000);
+        $('.imagem3').fadeIn(1000);
+        $('.imagem3').fadeOut(10000);
+        $('.imagem3').fadeIn(1000);
+        $('.imagem3').fadeIn(1000);
+        $('.imagem3').fadeOut(10000);
+        $('.imagem3').fadeIn(1000);
+        $('.imagem3').fadeOut(10000);
+        $('.imagem3').fadeIn(10000);
+        
+     });
+});
