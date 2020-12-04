49562147ad46cbf19fd273fbf9165799620509e6
keven-shtk <keven.elpidio@etec.sp.gov.br>
2020-12-04 01:30:43 -0300

    Commit

--- sw.js
+++ sw.js
@@ -1,20 +1,20 @@
-var cacheName = 'NomeApp-v';
+var cacheName = 'NameApp-v';
 var filesToCache = [
     './',
     './index.html',
     './manifest.json'
 ];
-self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(filesToCache);
-    })
-  );
+self.addEventListener('install', function(e){
+    e.waitUntil(
+        caches.open(cacheName).then(function(cache){
+            return cache.addAll(filesToCache);
+        })
+    );
 });
-self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    caches.match(e.request).then(function(response) {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', function(e){
+    e.respondWith(
+        caches.match(e.request).then(function(response){
+            return response || fetch(e.request);
+        })
+    );
 });
